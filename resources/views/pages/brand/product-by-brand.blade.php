78e6951a0494d0ea2acec18f2592af10847a97d1
Le Thanh Long <leethanhlong2210@gmail.com>
2023-06-13 17:28:04 +0700

    update detail page

--- resources/views/pages/brand/product-by-brand.blade.php
+++ resources/views/pages/brand/product-by-brand.blade.php
@@ -4,7 +4,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-5">
-                    <h1><b>Products</b></h1>
+                    <h1><b>Produccts</b></h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, quidem facere aspernatur natus
                         voluptatum ex sequi doloremque molestiae, sunt inventore est exercitationem quia accusamus esse
                         consectetur ipsam corporis, vitae.</p>
@@ -15,7 +15,7 @@
     <div class="product-body">
         <div class="container">
             <div class="row">
-                <div class="col-2">
+                <div class="col-3">
                     <div class="left-item category-item">
                         <h5>Category</h5>
                         @foreach($categories as $key => $category_item)
@@ -31,7 +31,7 @@
                         @endforeach
                     </div>
                 </div>
-                <div class="col-10">
+                <div class="col-9">
                     <div class="feature-product">
                         <div class="d-flex">
                             <div class="col-6">
@@ -48,14 +48,15 @@
                         </div>
                     </div>
                     <div class="product-section">
-                        @foreach($products as $key => $product_item)
+                        @foreach($products['data'] as $key => $product_item)
                             <form action="{{URL::to('/add-cart')}}" method="post">
                                 {{ csrf_field() }}
                                 @include('pages.common.product_item')
                             </form>
                         @endforeach
                     </div>
-                    <div class="pagination"></div>
+                    <div class="pagination">
+                    </div>
                 </div>
             </div>
         </div>
