f0172791bc5292e64be1d683706ed61c2e8eabe7
namaeb <leethanhlong7890@gmail.com>
2023-06-18 23:56:10 +0700

    update post image

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -129,13 +129,16 @@
                             <button type="submit" name="search" class="btn btn-sm"><i class="fa fa-search"></i></button>
                         </form>
                     </div>
+                    <div class="banner">
+                        <a href=""><img src="{{'WebPage/img/poster/poster1.jpg'}}" alt=""></a>
+                    </div>
                     <div class="tag-post">
                         @foreach($tags as $key => $tag_item)
                             @include('pages.common.tag_item')
                         @endforeach
                     </div>
                     <div class="banner">
-
+                        <a href=""><img src="{{'WebPage/img/poster/poster2.png'}}" alt=""></a>
                     </div>
                 </div>
             </div>
